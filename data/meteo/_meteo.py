2fcefaa198c12fe45276f6c645cb23903fdf6c32
BaptisteRdt <rodetbaptiste@gmail.com>
2024-12-08 21:33:07 +0100

    Meteo historical data

--- data/meteo/_meteo.py
+++ data/meteo/_meteo.py
@@ -1,6 +1,7 @@
 import openmeteo_requests
 import requests_cache
 import pandas as pd
+from datetime import datetime
 from retry_requests import retry
 
 
@@ -14,8 +15,8 @@ def get_forcast_weather(lat, lon) -> pd.DataFrame:
 	# The order of variables in hourly or daily is important to assign them correctly below
 	url = "https://api.open-meteo.com/v1/forecast"
 	params = {
-		"latitude": 52.52,
-		"longitude": 13.41,
+		"latitude": lat,
+		"longitude": lon,
 		"hourly": ["temperature_2m", "relative_humidity_2m", "soil_temperature_0cm", "soil_moisture_1_to_3cm"]
 	}
 	responses = openmeteo.weather_api(url, params=params)
@@ -44,3 +45,42 @@ def get_forcast_weather(lat, lon) -> pd.DataFrame:
 
 	return pd.DataFrame(data=hourly_data)
 
+
+def get_historical_weather(start_date: datetime, end_date: datetime, lat: float, lon: float) -> pd.DataFrame:
+	# Setup the Open-Meteo API client with cache and retry on error
+	cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
+	retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+	openmeteo = openmeteo_requests.Client(session=retry_session)
+
+	# Make sure all required weather variables are listed here
+	# The order of variables in hourly or daily is important to assign them correctly below
+	url = "https://archive-api.open-meteo.com/v1/archive"
+	params = {
+		"latitude": lat,
+		"longitude": lon,
+		"start_date": start_date.strftime("%Y-%m-%d"),
+		"end_date": end_date.strftime("%Y-%m-%d"),
+		"hourly": "temperature_2m"
+	}
+	responses = openmeteo.weather_api(url, params=params)
+
+	# Process first location. Add a for-loop for multiple locations or weather models
+	response = responses[0]
+	print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+	print(f"Elevation {response.Elevation()} m asl")
+	print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+	print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+	# Process hourly data. The order of variables needs to be the same as requested.
+	hourly = response.Hourly()
+	hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+
+	hourly_data = {"date": pd.date_range(
+		start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+		end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+		freq=pd.Timedelta(seconds=hourly.Interval()),
+		inclusive="left"
+	)}
+	hourly_data["temperature_2m"] = hourly_temperature_2m
+
+	return pd.DataFrame(data=hourly_data)
